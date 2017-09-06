90bdb861e2e117bc1f6c19acd4af74dc9a60c0b0
nishantdania <nishantdania@gmail.com>
2017-09-06 19:52:37 +0530

    old site

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.sendFile('public/index.html');
+})
+
+app.listen(4500, function () {
+
+})
